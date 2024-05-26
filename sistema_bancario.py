ff4925c2b8ba458e90f35e86499e7f2fafb643d4
Marcos Alberto <marcosjr_rj@hotmail.com>
2024-05-26 09:49:59 -0300

    Atualização do arquivo sistema_bacario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -7,13 +7,16 @@ menu = '''
 
 => '''
 
-saldo = 0
+
 limite = 500
-extrato = ''
+extrato = {
+    'lista_depositos' : [],
+    'lista_saques' : [],
+    'saldo' : 0
+}
 numero_saques = 0
-LIMTE_SAQUES = 3
-lista_depositos = []
-lista_saques = []
+LIMITE_SAQUES = 3
+
 
 while True:
 
@@ -28,44 +31,45 @@ while True:
                 break
             else:
                 print('Não é possível depositar valor negativo!')
-        saldo += qtd_deposito
-        lista_depositos.append(qtd_deposito)
+        extrato['saldo'] += qtd_deposito
+        extrato['lista_depositos'].append(qtd_deposito)
 
     elif opcao == 's':
         print('Saque')
         print('-=' * 20)
-        if numero_saques < LIMTE_SAQUES:            
+        if numero_saques < LIMITE_SAQUES:
             while True:
                 qtd_saque = float(input('Escolha o valor para sacar R$: '))                
                 if qtd_saque > 0  and qtd_saque <= 500:
                     break
                 else:
                     print('Não são permitidos valores negativos, ou valores acima de R$ 500,00')
-            if saldo >= qtd_saque:
-                saldo -= qtd_saque
-                lista_saques.append(qtd_saque)
+            if extrato['saldo'] >= qtd_saque:
+                extrato['saldo'] -= qtd_saque
+                extrato['lista_saques'].append(qtd_saque)
                 numero_saques += 1
                 print(f'O saque de R$ {qtd_saque:.2f} foi realizado')
-                print(numero_saques)
+
             else:
-                print(f'O seu saldo atual é de R$ {saldo:.2f}, portanto você não pode sacar a quantia de R$ {qtd_saque:.2f}')
+                print(f'O seu saldo atual é de R$ {extrato["saldo"]:.2f}, portanto você não pode sacar a quantia de R$ {qtd_saque:.2f}')
         else:
             print('Você já realizou o limite de 3 saques diários, aguarde o próximo dia.')
 
     elif opcao == 'e':
         print('Extrato detalhado')
         print('-=' * 20)
-        print(f'Você realizou {len(lista_depositos)} depósitos.')
-        if len(lista_depositos) > 0:
-            print('E eles foram no valor de :  ')
-            for valores in lista_depositos:
-                print(f'R$ {valores:.2f}')
-        print(f'Você realizou {len(lista_saques)} saques. ')
-        if len(lista_depositos) > 0:
-            print('E eles foram no valor de :  ')
-            for saque in lista_saques:
+        print(f'Você realizou {len(extrato["lista_depositos"])} depósito(s).')
+        if len(extrato['lista_depositos']) > 0:
+            for deposito in extrato['lista_depositos']:
+                print(f'R$ {deposito:.2f}')
+        print('-=' * 20)
+        print(f'Você realizou {len(extrato["lista_saques"])} saque(s).')
+        print('-=' * 20)
+        if len(extrato['lista_saques']) > 0:
+            for saque in extrato['lista_saques']:
                 print(f'R$ {saque:.2f}')
-        print(f'O seu salo final é de R$ {saldo:.2f}.')
+        print('-=' * 20)
+        print(f'O seu salo final é de R$ {extrato["saldo"]:.2f}.')
     
     elif opcao == 'q':
         break
